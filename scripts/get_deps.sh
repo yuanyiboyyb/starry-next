89e27b6f6fc2b6a68cda680711ae2bc8b0fbcff5
yfblock <321353225@qq.com>
2025-02-11 18:00:49 +0800

    modify: modify deps

--- scripts/get_deps.sh
+++ scripts/get_deps.sh
@@ -4,6 +4,6 @@ AX_ROOT=.arceos
 
 test ! -d "$AX_ROOT" && echo "Cloning repositories ..." || true
 # test ! -d "$AX_ROOT" && git clone https://github.com/arceos-org/arceos -b monolithic "$AX_ROOT" --depth=1 || true
-test ! -d "$AX_ROOT" && git clone https://github.com/Azure-stars/arceos -b feat_loongarch64 "$AX_ROOT" --depth=1 || true
+test ! -d "$AX_ROOT" && git clone https://github.com/oscomp/arceos -b feat_loongarch64 "$AX_ROOT" --depth=1 || true
 
 $(dirname $0)/set_ax_root.sh $AX_ROOT
